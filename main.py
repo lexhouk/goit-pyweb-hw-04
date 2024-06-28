6fe59b7e754dc3bb3f0476885988ebd44db965eb
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-06-28 15:38:51 +0300

    Issue #1: Add a handler for POST requests that save form data in a file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from mimetypes import guess_type
 from pathlib import Path
+from urllib.parse import unquote_plus, urlparse
+from json import dump, load
+from json.decoder import JSONDecodeError
+from datetime import datetime
 
 ROOT = Path('front-init')
 
@@ -9,7 +13,7 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self) -> None:
         code = 200
 
-        path = self.path
+        path = urlparse(self.path).path
         path = ROOT.joinpath('index.html' if path == '/' else path[1:])
 
         if not path.exists():
@@ -18,11 +22,37 @@ class Handler(BaseHTTPRequestHandler):
 
         self.send_response(code)
         self.send_header('Content-Type', guess_type(path)[0] or 'text/plain')
-
         self.end_headers()
 
         with open(path, 'rb') as file:
             self.wfile.write(file.read())
+
+    def do_POST(self) -> None:
+        if size := self.headers.get('Content-Length'):
+            items = self.rfile.read(int(size)).decode().split('&')
+            items = [item.split('=') for item in items]
+            items = {key: unquote_plus(value) for key, value in items if value}
+
+            if items:
+                if (path := ROOT.joinpath('storage/data.json')).exists():
+                    with open(path, encoding='utf-8') as file:
+                        try:
+                            data = load(file)
+                        except JSONDecodeError:
+                            ...
+
+                if 'data' not in locals() or type(data) is not dict:
+                    data = {}
+
+                # 2022-10-29 20:20:58.020261
+                data[datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')] = items
+
+                with open(path, 'w', encoding='utf-8') as file:
+                    dump(data, file, ensure_ascii=False, indent=2)
+
+        self.send_response(302)
+        self.send_header('Location', '/message.html')
+        self.end_headers()
 
 
 def main() -> None:
